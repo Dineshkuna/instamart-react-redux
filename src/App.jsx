a1e23966343ca9a9173d79a5ba51092fa18a6438
DineshKuna <kunadinesh.work@gmail.com>
2024-11-21 15:30:57 +0530

    version

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <p>Instamart-react-redux Project</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
