043818978dd6b5e1e21b69e86ff21e5b05dd767e
DineshKuna <kunadinesh.work@gmail.com>
2025-02-05 11:25:29 +0530

    version11

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,57 @@
+import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Footer.css";
 
-// import React from 'react'
-
-const Footer = () => {
+const FooterCard = () => {
   return (
     <div>
-      <p>Footer</p>
-    </div>
-  )
-}
+      <footer>
+        <div>
+          <p>e-Shop</p>
+          <p>
+            Your one-step for all your needs. Shop with use and experience the
+            best online shopping experience
+          </p>
+        </div>
+        <div>
+          <p>Quick Links</p>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+        <div>
+          <p>Follow us</p>
+          <p>
+            <FaFacebook />
+          </p>
+          <p>
+            <FaTwitter />
+          </p>
+          <p>
+            <FaGithub />
+          </p>
+          <p>
+            <FaLinkedin />
+          </p>
 
-export default Footer
+          <input type="email" placeholder="Enter Email" />
+          <button>Subscribe</button>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterCard;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,24 +2,23 @@ import GenCategory from "../genderCategory/GenderCategory";
 import Info from "../infoData/info";
 import HeroSlide from "../main/heroslide";
 import { setProducts } from "../redux/productSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux"; //
 import { useEffect } from "react";
 import ProductCard from "../productCard.jsx/ProductCard";
 import mockData from "../../assets/Images-main/Images-main/mockData";
 import Shop from "./Shop";
-
+import FooterCard from "../footer/Footer";
 
 
 const Home = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.products || []);
- 
 
   console.log("Products:", products); // Debugging
 
-  useEffect(() => {
+  useEffect(()=> {
     dispatch(setProducts(mockData));
-  }, [dispatch]);
+  },[dispatch]);
 
   return (
     <div>
@@ -28,21 +27,25 @@ const Home = () => {
       <GenCategory />
 
       <div className="container mx-auto py-12">
-        <h2 className="text-2xl font-bold mb-6 text-center" style={{textAlign:"center"}}>Top Products</h2>
+        <h2
+          className="text-2xl font-bold mb-6 text-center"
+          style={{ textAlign: "center" }}
+        >
+          Top Products
+        </h2>
 
         <div className="grid grid-cols-1 sm:grid-cole-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
           {Array.isArray(products) &&
-            products.slice(0,5).map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
+            products
+              .slice(0, 5)
+              .map((product) => (
+                <ProductCard key={product.id} product={product} />
+              ))}
         </div>
-
       </div>
 
-      <Shop/>
-
-      
-
+      <Shop />  
+      <FooterCard/> 
       
     </div>
   );
