55c74ae4b0f7b03f07bc12b4bcd4720920a55011
DineshKuna <kunadinesh.work@gmail.com>
2025-03-03 11:18:11 +0530

    version23_ContactPageUpdate

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,46 @@
-// import React from 'react'
+// import React from "react";
+import { FaMapMarkerAlt, FaPhoneAlt, FaEnvelope, FaGlobe } from "react-icons/fa";
+import "./Contact.css"; // Import the external CSS file
+import FooterCard from "../footer/Footer";
 
-const Contact = () => {
+const ContactCard = () => {
   return (
     <div>
-      <p>Contact</p>
-    </div>
-  )
-}
+    <div className="contact-container">
+      <div className="contact-card">
+        <h2 className="contact-title">Contact Us</h2>
+        <div className="contact-info">
+          {/* Address */}
+          <div className="contact-item">
+            <FaMapMarkerAlt className="contact-icon" />
+            <p>India, Hyderabad, PinCode 532345</p>
+          </div>
 
-export default Contact;
+          {/* Phone */}
+          <div className="contact-item">
+            <FaPhoneAlt className="contact-icon" />
+            <p>+91 8019555222</p>
+          </div>
+
+          {/* Email */}
+          <div className="contact-item">
+            <FaEnvelope className="contact-icon" />
+            <p>kunadinesh.work@gmail.com</p>
+          </div>
+
+          {/* Website */}
+          <div className="contact-item">
+            <FaGlobe className="contact-icon" />
+            <p>https://github.com/Dineshkuna/instamart-react-redux</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    
+      <FooterCard/>
+    </div>
+  );
+};
+
+export default ContactCard;
